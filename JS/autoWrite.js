67f0ddab38978d68de13e99cbd183edafab32dae
macs100 <lu.rgnr@gmail.com>
2023-03-29 08:37:45 -0300

    Initial commit 	new file:   CSS/aptitudes.css 	new file:   CSS/cabecera.css 	new file:   CSS/estudios.css 	new file:   CSS/hero.css 	new file:   CSS/main.css 	new file:   CSS/premios.css 	new file:   CSS/scrollDown.css 	new file:   Docs/Curriculum Regner.pdf 	new file:   IMG/heroImage.png 	new file:   IMG/heroImage2.png 	new file:   IMG/miFoto.jpg 	new file:   IMG/scroll-bar.png 	new file:   JS/autoWrite.js 	new file:   SVG/free_icon_1(1).svg 	new file:   SVG/free_icon_1.svg 	new file:   SVG/icons8-scroll-down-100.png 	new file:   SVG/icons8-scroll-down-50.png 	new file:   index.html

---
+++ JS/autoWrite.js
@@ -0,0 +1,43 @@
+typeWriterEffect()
+
+function typeWriterEffect(){
+    const words = ['Técnico informático', 'programador', 'desarrollador web', 'estudiante', 'autodidacta'];
+    let wordCount = 0;
+    let letterCount = 0;
+    let currentText = '';
+    let currentWord = '';
+    let timeOut = 400;
+    let isDeleting = false;
+
+    type()
+
+    function type(){
+        if(wordCount === words.length){
+            wordCount = 0;
+        }
+        currentWord = words[wordCount];
+
+        if(isDeleting){
+            currentText = currentWord.slice(0, --letterCount)
+        } else {
+            currentText = currentWord.slice(0, ++letterCount)
+        }
+        
+        document.querySelector('.typewrite').textContent = currentText;
+
+        timeOut = isDeleting? 50:100;
+
+        if (!isDeleting && currentText.length === currentWord.length){
+            timeOut = 2000;
+            isDeleting = true
+        } else if (isDeleting && currentText.length === 0){
+            timeOut = 500;
+            isDeleting = false;
+            wordCount++;
+        }
+        setTimeout(type, timeOut)
+
+
+    }
+   
+}
