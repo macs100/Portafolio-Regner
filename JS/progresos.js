e8199cfbe800faf561b9eaf87f73d32f6d8e287d
macs100 <lu.rgnr@gmail.com>
2023-04-04 18:22:12 -0300

    last Commit

---
+++ JS/progresos.js
@@ -0,0 +1,14 @@
+
+
+let progresos = document.getElementsByClassName('progreso')
+
+let lista = Array.from(progresos)
+
+
+for (let i = 0; i < lista.length; i++) {
+    const element = lista[i];
+
+    
+    
+}
+lista[i]
